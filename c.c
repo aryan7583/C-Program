c9e6afbd22f64db3aa4919522df38b9910901d58
aryan7583 <123481900+aryan7583@users.noreply.github.com>
2023-01-25 18:51:34 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,20 @@
+
+// 'C' Program on simple loop concept
+# include<stdio.h>
+int main(){
+    int age;
+    printf("enter age: ");
+    scanf("%d",&age);
+if(age>18){
+    printf("person is adult \n");
+    printf("person is able to vote for nation \n");
+    printf("person is able to drive vehicle \n");
+}
+else{
+    printf("Person is not adult(Child) \n");
+    printf("person is not able to vote for nation \n");
+    printf("person is not able to drive vehicle \n");
+
+}
+return 0;
+}
